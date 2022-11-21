901ff967f6837d8c5a02f9683e2266b9455ec08a
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-21 18:18:35 -0300

    Resolucao do exercicio 4 de objetos, criando um objeto e chamando seu metodo

--- exercicios-poo/src/App.java
+++ exercicios-poo/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Carro carro = new Carro();
+        carro.setCor("branco");
+        carro.setModelo("Honda Fit");
+        carro.setCapacidadeTanque(51);
+
+        System.out.println("O modelo do carro é: " + carro.modelo);
+        System.out.println("A cor do carro é: " + carro.cor);
+        System.out.println("A capacidade do tanque do carro é: " + carro.capacidadeTanque + "L");
+        System.out.println("Com a gasolina a R$5.00, você gastará  " + carro.calcularTotal(5) + " reais para encher o tanque.");
     }
 }
