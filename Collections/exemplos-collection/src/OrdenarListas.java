54ae447b0674545e8fa44d3cd5a1bf15ba3ea0ff
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-22 22:10:26 -0300

    Trabalhando com comparator

--- Collections/exemplos-collection/src/OrdenarListas.java
+++ Collections/exemplos-collection/src/OrdenarListas.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 // Dadas as seguintes informações sobre gatos, crie uma lista
@@ -24,6 +25,16 @@ public class OrdenarListas {
         System.out.println(meusGatos);
         //mostrando em ordem natural do nome
         Collections.sort(meusGatos);
+        System.out.println(meusGatos);
+        //mostrando em ordem de idade
+        Collections.sort(meusGatos, new ComparatorIdade());
+        //meusGatos.sort(new ComparatorIdade());
+        System.out.println(meusGatos);
+        //mostrando em ordem de cor
+        meusGatos.sort(new ComparatorCor());
+        System.out.println(meusGatos);
+        //ordenando por Nome/Cor/Idade
+        meusGatos.sort(new ComparatorNomeCorIdade());
         System.out.println(meusGatos);
     }
 }
@@ -76,4 +87,36 @@ class Gato implements Comparable<Gato>{
         return this.getNome().compareToIgnoreCase(gato.getNome());
     }
     
+}
+class ComparatorIdade implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        return Integer.compare(gato1.getIdade(), gato2.getIdade());
+        
+    }
+}
+
+class ComparatorCor implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        return gato1.getCor().compareToIgnoreCase(gato2.getCor());
+    }
+
+} 
+
+class ComparatorNomeCorIdade implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+        int nome = o1.getNome().compareToIgnoreCase(o2.getNome());
+        if (nome!=0) return nome;
+
+        int cor = o1.getCor().compareToIgnoreCase(o2.getCor());
+        if (cor!=0) return cor;
+
+        return Integer.compare(o1.getIdade(), o2.getIdade());
+    }
+
 }
